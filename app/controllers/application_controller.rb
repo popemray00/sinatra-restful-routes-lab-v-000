6dd0fe32499b7212bb0192d9eb388436693e85e4
Michael Pope <popemray1986@gmail.com>
2018-05-28 18:48:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  get '/' do 
+    redirect to '/recipes'
+  end
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
